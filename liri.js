68ba23856f5404e55c7ebdc27ffb61b0b7f9b010
Nancy Harris <nanzyharris@gmail.com>
2019-11-19 17:45:15 -0600

    liri.js

--- liri.js
+++ liri.js
@@ -27,7 +27,6 @@ var fullCommand = [];
 let command = process.argv[2];
 let input = process.argv[3];
 
-
 // Add switch commands - switch is a keyword, not a variable or function
 switch (command) {
     case "help":
@@ -68,8 +67,9 @@ function instructions() {
 }
 
 // Write a function to record search results into log.txt
-function logToFile(value) {
-    fs.appendFile(fileName, ',' + value, function (err) {
+function logToFile(command, input) {
+    //   fullCommand.push(command, input)
+    fs.appendFile(fileName, ',' + command + input, function (err) {
         if (err) {
             return console.log("Error found, but don't panic, it's not fatal")
         }
@@ -77,7 +77,7 @@ function logToFile(value) {
 }
 
 // Call function logToFile to record search results
-logToFile(fullCommand);
+logToFile();
 
 // Write function "concert_this" to search through Bands In Town
 function concert_this() {
@@ -85,7 +85,7 @@ function concert_this() {
         input = "Bob Dylan"
     }
 
-    Axios.get(`https://rest.bandsintown.com/artists/` + artist + `/events?app_id=codingbootcamp`)
+    Axios.get(`https://rest.bandsintown.com/artists/` + input + `/events?app_id=codingbootcamp`)
         .then(function (response) {
             // Print data to console with console.log
             console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
@@ -95,7 +95,7 @@ function concert_this() {
             console.log(`Venue Name: ${response.data[0].venue.name}`)
             console.log(`Venue Region: ${response.data[0].venue.name}`)
             // Use moment.js to format the date
-            console.log(`Show date: ${Moment(response.data[0])}`)
+            console.log(`Show date: ${Moment(response.data[0]).format("MM-DD-YYYY")}`)
             console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
         });
 }
@@ -108,7 +108,7 @@ function spotify_this() {
             return console.log('Error occurred: ' + err);
         }
         console.log(data.tracks.items[0].name);
-        console.log(data.tracks.items[0].artist.name);
+        console.log(data.tracks.items[0].artists[0].name);
         console.log(data.tracks.items[0].album.name);
         console.log(data.tracks.items[0].preview_url);
     });
@@ -120,7 +120,7 @@ function movie_this() {
         input = "Mr. Nobody"
     }
 
-    Axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=59d24f2f`)
+    Axios.get(`http://www.omdbapi.com/?t=${input}&apikey=59d24f2f`)
         .then(function (response) {
             // Print data to console with console.log
             console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
@@ -132,7 +132,6 @@ function movie_this() {
             console.log(`Language: ${response.data.Language}`)
             console.log(`Plot: ${response.data.Plot}`)
             console.log(`Actors: ${response.data.Actors}`)
-
             console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
         });
 }
@@ -142,8 +141,36 @@ function do_what_it_says() {
         if (error) {
             return console.log(error);
         }
-        spotify.search({ type: 'track', query: input, limit: 1 }, function (err, data) {
-
-        });
-    })
+        else {
+            var dataArray = data.split(", ");
+            command = dataArray[0]
+            input = dataArray[1]
+            console.log(dataArray, command, input);
+            switch (command) {
+                case "help":
+                    // Call the function for instructions
+                    instructions();
+                case "concert-this":
+                    // Call the function for concert-this. Notice the function name has an underscore instead of hyphen
+                    concert_this();
+                    break;
+                case "spotify-this":
+                    // Call the function for spotify-this. Notice underscore instead of hyphen
+                    spotify_this();
+                    break;
+                case "movie-this":
+                    // Call the funtion movie-this. Notice underscore instead of hyphen
+                    movie_this();
+                    break;
+                case "default":
+                    // Instructions to indicate user what to type on command line 
+                    console.log("Type any of the following commands after \"node liri.js:\" " +
+                        "\n" + "concert-this 'artist/band name' " +
+                        "\n" + "spotify-this 'song title' " +
+                        "\n" + "movie-this 'movie title' " +
+                        "\n" + "do-what-it-says ");
+                    break;
+            };
+        }
+    });
 }
